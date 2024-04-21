4662ec379956272e93a43c77008723c95ad5debe
AhshanHabib26 <habib.net27@gmail.com>
2024-04-21 09:00:10 +0600

    User Order Page Added

--- src/components/Dashboard/SideNavbar.tsx
+++ src/components/Dashboard/SideNavbar.tsx
@@ -1,12 +1,10 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
 import Logo from "../../assets/icons/logo.png";
 import Image from "next/image";
 import { SideMenuItems } from "@/utils/SideMenuItems";
